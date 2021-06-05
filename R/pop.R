3a4950e35d7eb42c5a8debd685961ced366d6992
Max Salvatore <maxsalvatore@Maxs-MacBook-Air.local>
2021-06-05 17:51:43 -0400

    add population data

---
+++ R/pop.R
@@ -0,0 +1,11 @@
+#' List of places, abbreviations, and populations
+#'
+#'
+#' @format A data frame with 3 variables: \code{place}, \code{abbrev},
+#'   \code{population}
+#' \describe{
+#' \item{place}{The name of the place}
+#' \item{abbrev}{The abbreviations corresponding to place}
+#' \item{population}{The population size}
+#' }
+"pop"
