06790381ecf1d4a38476821f093970217e6af965
Max Salvatore <maxsalvatore@Maxs-MacBook-Air.local>
2021-06-05 19:02:16 -0400

    update documentation

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -5,8 +5,8 @@
 #' @name %>%
 #' @rdname pipe
 #' @keywords internal
-#' @importFrom magrittr %>%
 #' @export
+#' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
 #' @param lhs A value or the magrittr placeholder.
 #' @param rhs A function call using the magrittr semantics.
