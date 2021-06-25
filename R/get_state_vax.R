5154653792aac261981081f5453df19811ded4be
Max Salvatore <maxsalvatore@Maxs-MacBook-Air.local>
2021-06-25 12:30:50 -0400

    update vaccine data function

--- R/get_state_vax.R
+++ R/get_state_vax.R
@@ -1,5 +1,5 @@
 #' Pull covid19india state-level vaccine data
-#' @param path The URL path for the data. Default: https://api.covid19india.org/csv/latest/cowin_vaccine_data_statewise.csv
+#' @param path The URL path for the data. Default: https://api.covid19india.org/csv/latest/vaccine_doses_statewise_v2.csv
 #' @param raw Pull raw unaltered data. Default is FALSE
 #' @param keep_nat Keep national level data? Default is TRUE
 #' @return Pulls the time-series state-level vaccine data directly from covid19india.org.
@@ -12,13 +12,13 @@
 #' }
 
 get_state_vax <- function(
-  path       = "https://api.covid19india.org/csv/latest/cowin_vaccine_data_statewise.csv",
+  path       = "https://api.covid19india.org/csv/latest/vaccine_doses_statewise_v2.csv",
   raw        = FALSE,
   keep_nat   = TRUE
 ) {
 
   if (keep_nat == TRUE) {
-    message("Also pulling national level data under `state = \"India\"`. Set `keep_nat == FALSE` to exclude by default")
+    message("Also pulling national level data under `place = \"India\"`. Set `keep_nat == FALSE` to exclude by default")
   }
 
   d <- suppressWarnings(readr::read_csv(path,
@@ -29,26 +29,31 @@ get_state_vax <- function(
     d <- d %>%
       janitor::clean_names() %>%
       dplyr::mutate(
-        date = as.Date(updated_on, "%d/%m/%Y")
+        date = as.Date(vaccinated_as_of, "%d/%m/%Y")
       ) %>%
-      dplyr::select(
-        date, place = state,
-        total_vax_ppl = total_individuals_vaccinated,
-        total_doses   = total_doses_administered,
-        first_doses   = first_dose_administered,
-        second_doses  = second_dose_administered
+      dplyr::select(place = state, date,
+                    first_dose = first_dose_administered,
+                    second_dose = second_dose_administered,
+                    total_doses = total_doses_administered) %>%
+      dplyr::mutate(
+        place = dplyr::case_when(place == "Total" ~ "India", T ~ place)
       ) %>%
-      dplyr::group_by(state) %>%
+      dplyr::left_join(pop %>%
+                         dplyr::select(-abbrev) %>%
+                         dplyr::add_row(place = "Dadra and Nagar Haveli and Daman and Diu",
+                                        population = 3.44e5 + 2.43e5),
+                       by = "place") %>%
+      dplyr::mutate(
+        pct_one_doses = round(first_dose * 100 / population, 4),
+        pct_two_doses = round(second_dose * 100/ population, 4)
+      ) %>%
+      dplyr::select(-population) %>%
+      dplyr::group_by(place) %>%
+      dplyr::arrange(date) %>%
       dplyr::mutate(
         daily_doses = total_doses - dplyr::lag(total_doses)
       ) %>%
-      dplyr::ungroup() %>%
-      dplyr::left_join(pop %>% dplyr::select(-abbrev), by = "place") %>%
-      dplyr::mutate(
-        pct_one_doses = round(total_vax_ppl * 100 / population, 4),
-        pct_two_doses = round(second_doses * 100/ population, 4)
-      ) %>%
-      dplyr::select(-population)
+      ungroup()
 
   }
 
