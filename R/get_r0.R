de0b36d481071344d56ca95084194a882a8112f7
Max Salvatore <maxsalvatore@Maxs-MacBook-Air.local>
2021-06-28 16:38:50 -0400

    fix incidence variable specification in r0 caluclation

--- R/get_r0.R
+++ R/get_r0.R
@@ -17,7 +17,7 @@ estR0_out <- function(dat, inc_var) {
   t_end   <- t_start + 4
 
   res <- EpiEstim::estimate_R(
-    incid = dat[[{{ inc_var }}]],
+    incid = dat[[inc_var]],
     method = "parametric_si",
     config = EpiEstim::make_config(list(
       mean_si             = 7,
@@ -86,7 +86,7 @@ get_r0 <- function(
     dplyr::select(date, daily_cases = {{ inc_var }}, place) %>%
     tidyr::nest(data = c(-place)) %>%
     dplyr::mutate(
-      estR0 = purrr::map(data, ~covid19india:::estR0_out(dat = .x, inc_var = inc_var))
+      estR0 = purrr::map(data, ~covid19india:::estR0_out(dat = .x, inc_var = deparse(substitute(inc_var))))
     ) %>%
     tidyr::unnest(estR0) %>%
     dplyr::select(-data) %>%
