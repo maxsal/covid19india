34a12bdc45e68dc74e5452135d89d6c5e3e54eda
Max Salvatore <maxsalvatore@Maxs-MacBook-Air.local>
2021-10-26 13:37:57 -0400

    fix typo

--- R/get_r_est.R
+++ R/get_r_est.R
@@ -10,7 +10,7 @@
 
 get_r_est <- function(x) {
 
-  x[x[order(date), .SD[ifelse(.N-6 < 1, 1, .N-6):.N], by = "place"]][, .(
+  x[order(date), .SD[ifelse(.N-6 < 1, 1, .N-6):.N], by = "place"][, .(
     r     = mean(r_est, na.rm = TRUE),
     lower = mean(r_lower, na.rm = TRUE),
     upper = mean(r_upper, na.rm = TRUE)),
